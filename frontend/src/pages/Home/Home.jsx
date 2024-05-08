3f670028ea1f48d07d6c066079fc4c275ff18451
ahmadravian47 <ahmad.ravian47@gmail.com>
2024-05-08 21:20:37 +0500

    Your commit message here

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero';
 import Sedan from '../../components/Sedan/Sedan';
+import Listing from '../../components/Listing/Listing'
 
 const Home = () => {
     return (
         <>
             <Hero />
             <Sedan />
+            <Listing></Listing>
         </>
     )
 }
