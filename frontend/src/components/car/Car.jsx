bf528bc77e36846ffaf870511775644832ec21e0
ahmadravian47 <ahmad.ravian47@gmail.com>
2024-05-15 22:36:35 +0500

    More changes

--- frontend/src/components/car/Car.jsx
+++ frontend/src/components/car/Car.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import '../car/Car.css'
-import car_image from '../../assets/car.jpg'
+import Accord from '../../assets/accord.png'
+import H6 from '../../assets/Havel.png'
+import Civic from '../../assets/Civicx.png'
+import Reborn from '../../assets/Reborn2.png'
+import Vezel from '../../assets/Vezel2.png'
+import Grande from '../../assets/g.png'
 import { Link } from 'react-router-dom';
 
+// Create a mapping of model names to image paths
+const carImages = {
+  Accord: Accord,
+  H6: H6,
+  Civic: Civic,
+  Reborn: Reborn,
+  Vezel: Vezel,
+  Grande: Grande
+};
 
 export default function Car(props) {
+  // Get the image source from the mapping
+  const imageSrc = carImages[props.model];
+
   return (
     <div id="carbox">
-      <img src={car_image}></img>
+      <img src={imageSrc} alt={`${props.make} ${props.model}`} />
       <h2>{props.make} {props.model}</h2>
       <h6>PKR {props.price}</h6>
       <div className='row'>
@@ -16,15 +33,13 @@ export default function Car(props) {
           <h5>{props.year}</h5>
         </div>
         <div className='column'>
-          <h5 className='bold'>Milleage</h5>
+          <h5 className='bold'>Mileage</h5>
           <h5>{props.mileage}</h5>
         </div>
       </div>
-      <Link to={`/car/${props.id}`} className='car_details' 
-            >
-              See Details
-            </Link>
-
+      <Link to={`/car/${props.id}`} className='car_details'>
+        See Details
+      </Link>
     </div>
   )
 }
