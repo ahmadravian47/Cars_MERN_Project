c58b0612641ff2e7280caaa726a1c462575240c8
ahmadravian47 <ahmad.ravian47@gmail.com>
2024-05-16 23:57:51 +0500

    More changes in frontend

--- frontend/src/components/Car_detail/C_detail.jsx
+++ frontend/src/components/Car_detail/C_detail.jsx
@@ -9,16 +9,33 @@ export default function C_detail() {
   const [image2, setImage2] = useState('');
   const [image3, setImage3] = useState('');
 
+  let user={//fetch fromZ
+    "_id": "user_id_2",
+    "username": "user456",
+    "email": "user456@example.com",
+    "password": "hashed_password_2",
+    "name": "Jane Smith",
+    "phone": "987-654-3210",
+    "location": "City B, Country B",
+    "joinedAt": {
+      "$date": "2024-05-02T00:00:00.000Z"
+    },
+    "__v": 0
+  }
+
   useEffect(() => {
     const fetchCar = async () => {
-      try {
-        const response = await fetch(`http://localhost:5000/car/${id}`);
-        const result = await response.json();
-        console.log(result)
-        setCar(result);
-      } catch (err) {
+        const response = await fetch(`http://localhost:5000/car/${id}`)
+        .then((res)=>{
+          return res.json();
+        })
+        .then((result)=>{
+          setCar(result)
+          console.log(result)
+        })
+        .catch ((err)=> {
         console.log('Error : ', err);
-      }
+      })
     };
     fetchCar();
   }, [id]);
@@ -56,9 +73,41 @@ export default function C_detail() {
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="sr-only">Next</span>
           </a>
+          <div className='detail_box'>
+          <h1>PKR {car.price}</h1>
+          <h5>{car.make} {car.model}</h5>
+          </div>
+          <div className='detail_box detail_box2 '>
+          
+
+            <h6>Make</h6>
+            <h6>Model</h6>
+            <h6>Type</h6>
+            <h6>Year</h6>
+            <h6>Mileage</h6>
+            <h6>{car.make}</h6>
+            <h6>{car.model}</h6>
+            <h6>{car.cartype}</h6>
+            <h6>{car.year}</h6>
+            <h6>{car.mileage}</h6>
+            <h3>Price</h3>
+            <h6>Condition</h6>
+            <h6>Fuel Type</h6>
+            <h6>Transmission</h6>
+            <h6>Color</h6>
+            <h6>{car.price}</h6>
+            <h6>{car.consition}</h6>
+            <h6>{car.fuelType}</h6>
+            <h6>{car.transmission}</h6>
+            <h6>{car.color}</h6>
+          </div>
         </div>
         <div className='child2'>
-          <h1>{car.model}</h1>
+          <h2>Seller Details</h2>
+         <h6>Seller Name : {user.username}</h6>
+         <h6>Seller Location : {user.location}</h6>
+         <h6>Seller Phone Number : {user.phone}</h6>
+         <a>Book an Appointment</a>
         </div>
       </div>
     </>
