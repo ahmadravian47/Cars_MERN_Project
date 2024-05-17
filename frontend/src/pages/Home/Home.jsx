b376b6154aa79fa9a98ddf295c3ce6961739a79d
ahmadravian47 <ahmad.ravian47@gmail.com>
2024-05-17 15:40:20 +0500

    changes

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -8,9 +8,10 @@ const Home = () => {
     return (
         <>
             <Hero />
+            <Service />
             <Listing></Listing>
             <Sedan />
-            <Service />
+           
         </>
     )
 }
